9eeb04a8546152249cd827634459532ae246225b
Philippe Collignon <phc@starobject.com>
2025-03-27 14:34:13 +0100

    Redis+JavaMelody tested in docker and k8s

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,6 +1,8 @@
 mvn clean install -Denvironment=ossdocker
 cp target/eusurvey.war docker/server/dist/eusurvey.war
 cd docker
+docker rmi ghcr.io/circabc/tomcat-eusurvey-server-k8s            
+docker rmi eusurvey-tomcat   
 docker-compose build
 docker tag eusurvey-tomcat ghcr.io/circabc/tomcat-eusurvey-server-k8s:latest
 docker push  ghcr.io/circabc/tomcat-eusurvey-server-k8s:latest
